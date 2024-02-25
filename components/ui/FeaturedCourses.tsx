c237648a0408e4f03c3d3323eea0f557cc828788
mateenshamsi <matinshamsi16@gmail.com>
2024-02-25 12:00:56 +0530

    first commit

---
+++ components/ui/FeaturedCourses.tsx
@@ -0,0 +1,53 @@
+"use client";
+import React from "react";
+import courseData from "../../data/music_courses.json";
+import Link from "next/link";
+import { BackgroundGradient } from "./background-gradient";
+interface Course {
+  id: number;
+  title: string;
+  slug: string;
+  description: string;
+  price: number;
+  instructor: string;
+  isFeatured: boolean;
+}
+
+function FeaturedCourses() {
+  const featuredCourses = courseData.courses.filter(
+    (course: Course) => course.isFeatured
+  );
+  return (
+    <div className="py-12 bg-gray-900">
+      <div>
+        <div className="text-center">
+          <h2 className="uppercase text-teal-500 text-base tracking-wide  font-semibold ">
+            Featured Courses
+          </h2>
+          <p className="uppercase  text-base tracking-wide  font-semibold ">
+            Learn with the best
+          </p>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center ">
+        {featuredCourses.map((course: Course) => (
+          <div key={course.id} className="flex justify-center">
+            <BackgroundGradient className="flex flex-col rounded-[22px]  p-4 sm:p-10 bg-white overflow-hidden h-full max-w-sm  dark:bg-zinc-900">
+              <div className="flex flex-col p-4 sm:p-6 items-center overflow-hidden text-center  ">
+                <p>{course.title}</p>
+                <p>{course.description}</p>
+                <Link href={`/courses/${course.slug}`}>
+                </Link>
+              </div>
+            </BackgroundGradient>
+          </div>
+        ))}
+      </div>
+      <div className="mt-20 text-center">
+        <Link href={"/courses"}>View All Courses</Link>
+      </div>
+    </div>
+  );
+}
+
+export default FeaturedCourses;
---
+++ components/ui/WhyChooseUs.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { StickyScroll } from './sticky-scroll-reveal'
+const content = [ {
+    title: "Guitar Fundamentals",
+    description: "Learn the basics of playing guitar with our comprehensive beginner's course.",
+  },
+  {
+    title: "Piano for Beginners",
+    description: "Start your musical journey with foundational piano skills taught by expert instructors.",
+   },
+  {
+    title: "Advanced Vocal Techniques",
+    description: "Enhance your singing with advanced vocal techniques for intermediate to advanced learners.",
+   },
+  {
+    title: "Drumming Mastery",
+    description: "Master the drums with our comprehensive course covering all skill levels.",
+   },
+  {
+    title: "Jazz Improvisation",
+    description: "Learn the art of jazz improvisation with this course designed for all levels.",
+   },
+  {
+    title: "Music Production Fundamentals",
+    description: "Dive into music production with this introductory course on the basics of sound engineering and mixing.",
+   },
+  {
+    title: "Songwriting Essentials",
+    description: "Learn the essentials of songwriting to express your musical creativity.",
+   },
+  {
+    title: "Electronic Music Production",
+    description: "Create compelling electronic music with our course designed for beginners to advanced users.",
+   },
+  {
+    title: "Classical Music History",
+    description: "Explore the rich history of classical music from its origins to the present day.",
+   },
+  {
+    title: " Blues Guitar Techniques",
+    description: "Discover the techniques of blues guitar to add soul and depth to your playing.",
+    }] 
+function WhyChooseUs() {
+  return (
+    <div>
+      <StickyScroll content={content}></StickyScroll>
+    </div>
+  )
+}
+
+export default WhyChooseUs
